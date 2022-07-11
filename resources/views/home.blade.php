119f96dd90c022f0eb3515aa20bfc93edecc04a7
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-07-11 17:32:58 +0200

    finalizzazione ex + bonus

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- styleshett css --}}
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Primi passi Laravel</title>
+</head>
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">HOME</a>
+                </li>
+                <li>
+                    <a href="/about">ABOUT</a>
+                </li>
+                <li>
+                    <a href="/contact">CONTACT</a>
+                </li>
+                <li>
+                    <a href="/join-us">JOIN US</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <h1>
+                Hello World!!! &hearts;
+            </h1>
+        </div>
+    </main>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,26 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+// route home
 Route::get('/', function () {
-    return view('hello');
+    return view('home');
 });
+
+// route about
+Route::get('/about', function () {
+    return view('about');
+});
+
+// route contact
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// route join us
+Route::get('/join-us', function () {
+    return view('join-us');
+});
+
+
 
 
